4fb8d83c926efc0c6bbb8416c527217100fa746c
Aleksander_K <aleks_sht@mail.ru>
2024-11-15 06:11:50 +0300

    add description and price fields

--- src/components/content/deserts/dessertDescriptionWindowComponent.js
+++ src/components/content/deserts/dessertDescriptionWindowComponent.js
@@ -1,7 +1,9 @@
 import { useDescriptionDessertsState } from "../../../store/actions/descriptionDessertsAction"
+import CurrencyRubleIcon from '@mui/icons-material/CurrencyRuble';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 
-export const DessertDescriptionWindowComponent = ({ name, img, description, button }) => {
+
+export const DessertDescriptionWindowComponent = ({ name, img, description, price, button }) => {
     const { closeAllDessertDescription } = useDescriptionDessertsState()
 
     return <div className="DessertDescriptionWindowComponent ">
@@ -11,8 +13,12 @@ export const DessertDescriptionWindowComponent = ({ name, img, description, butt
         >{<HighlightOffIcon style={{ fontSize: '3rem', color: '#4a1a1a', borderRadius: "25% 25%" }} />}</div>
 
         <img src={img} alt="description" />
-        {name}
-        {description}
+        <h1>{name}</h1>
+        <div>
+            <div className="ingredients">Состав:</div>
+            <div className="dessertsDescription">{description}</div>
+        </div>
+        <div className="price">{price} <CurrencyRubleIcon /></div>
         <div className="DessertsChildbutton ContactChildComponent">
             <span>{button}</span>
         </div>
--- src/components/content/deserts/dessetsComponent.js
+++ src/components/content/deserts/dessetsComponent.js
@@ -18,12 +18,12 @@ export const DessertsContantCompanent = () => {
     const descriptionDessertsState = useSelector(state => state.dessertsDescriptionState)
 
     const showDescriptionDessertWindow = () => {
-        if (descriptionDessertsState[0]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[0].name} img={assortimentDesertsArr[0].img} button={assortimentDesertsArr[0].button} />
-        if (descriptionDessertsState[1]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[1].name} img={assortimentDesertsArr[1].img} button={assortimentDesertsArr[1].button} />
-        if (descriptionDessertsState[2]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[2].name} img={assortimentDesertsArr[2].img} button={assortimentDesertsArr[2].button} />
-        if (descriptionDessertsState[3]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[3].name} img={assortimentDesertsArr[3].img} button={assortimentDesertsArr[3].button} />
-        if (descriptionDessertsState[4]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[4].name} img={assortimentDesertsArr[4].img} button={assortimentDesertsArr[4].button} />
-        if (descriptionDessertsState[5]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[5].name} img={assortimentDesertsArr[5].img} button={assortimentDesertsArr[5].button} />
+        if (descriptionDessertsState[0]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[0].name} img={assortimentDesertsArr[0].img} button={assortimentDesertsArr[0].button} description={assortimentDesertsArr[0].description} price={assortimentDesertsArr[0].price} />
+        if (descriptionDessertsState[1]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[1].name} img={assortimentDesertsArr[1].img} button={assortimentDesertsArr[1].button} description={assortimentDesertsArr[1].description} price={assortimentDesertsArr[1].price} />
+        if (descriptionDessertsState[2]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[2].name} img={assortimentDesertsArr[2].img} button={assortimentDesertsArr[2].button} description={assortimentDesertsArr[2].description} price={assortimentDesertsArr[2].price} />
+        if (descriptionDessertsState[3]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[3].name} img={assortimentDesertsArr[3].img} button={assortimentDesertsArr[3].button} description={assortimentDesertsArr[3].description} price={assortimentDesertsArr[3].price} />
+        if (descriptionDessertsState[4]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[4].name} img={assortimentDesertsArr[4].img} button={assortimentDesertsArr[4].button} description={assortimentDesertsArr[4].description} price={assortimentDesertsArr[4].price} />
+        if (descriptionDessertsState[5]) return <DessertDescriptionWindowComponent name={assortimentDesertsArr[5].name} img={assortimentDesertsArr[5].img} button={assortimentDesertsArr[5].button} description={assortimentDesertsArr[5].description} price={assortimentDesertsArr[5].price} />
 
     }
 
@@ -41,36 +41,48 @@ export const DessertsContantCompanent = () => {
             name: "Медовик",
             img: medovik,
             button: addButtonText,
+            description: "медовые коржи, сметанный крем с орехами, сметанный крем, медовые кейк-попсы",
+            price: 3500,
             clickFunc: medovikActive,
         },
         {
             name: "Кофейный",
             img: coffe,
             button: addButtonText,
+            description: "шоколадный бисквит, сливочная пропитка, крем «кофейный», заварной бисквит",
+            price: 3400,
             clickFunc: coffeActive,
         },
         {
             name: "Вишня в сметане",
             img: cherryInSourCream,
             button: addButtonText,
+            description: "бисквит с корицей, пропитка сливочная, вишневая начинка,сметанный мусс, сливочный крем",
+            price: 4300,
             clickFunc: cherryInSourCreamActive,
         },
         {
             name: "Французский поцелуй",
             img: franceKiss,
             button: addButtonText,
+            description: "песочные коржи, шоколадный ганаш, сливочный крем, карамельные фрукты",
+            price: 5100,
             clickFunc: franceKissActive,
         },
         {
             name: "Шоколад",
             img: chocolate,
             button: addButtonText,
+            description: "шоколадно-кофейный бисквит, шоколадный мусс",
+            price: 2700,
             clickFunc: chocolateActive,
         },
         {
             name: "Рулет «Шоколадный»",
             img: rulette,
             button: addButtonText,
+            description: "шоколадно-заварной бисквит, желе «вишня», сливочная пропитка, сливочный крем",
+            price: 2100,
             clickFunc: ruletteActive,
         },
     ]
