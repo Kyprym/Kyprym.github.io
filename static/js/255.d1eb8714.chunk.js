b9800124bffc411ca30a5bf8dce4a50751fe48d1
Aleksander_K <aleks_sht@mail.ru>
2024-12-22 18:30:29 +0300

    Updates

---
+++ static/js/255.d1eb8714.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknatali=self.webpackChunknatali||[]).push([[255],{255:(a,e,s)=>{s.r(e),s.d(e,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=255.d1eb8714.chunk.js.map
---
+++ static/js/76.3349007d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknatali=self.webpackChunknatali||[]).push([[76],{76:(a,e,s)=>{s.r(e),s.d(e,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=76.3349007d.chunk.js.map
